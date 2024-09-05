77f3782cc2b25990efd472617fc396144c133079
codewithsuraj <mail2surajmahendrakar@gmail.com>
2024-09-05 15:20:43 +0530

    ListGroup component is added

--- src/Message.tsx
+++ src/Message.tsx
@@ -2,5 +2,11 @@
 // PascalCasing
 function Message() {
     // JSX: JavaScript XML
-    return <h1>Hello World</h1>;
+
+    const name = 'Suraj';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
 }
+
+export default Message;
