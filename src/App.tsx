d046949c964d14f9f05f67cf1f04f727e11ea515
codewithsuraj <mail2surajmahendrakar@gmail.com>
2024-09-11 10:39:01 +0530

    Building a Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import "./App.css";
-import ExpandableText from "./components/ExpandableText/ExpandableText";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText maxChars={100}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita dolor
-        dolorem illo esse corporis minima explicabo nulla sit saepe ex iure
-        libero eligendi corrupti molestiae culpa in pariatur facilis fuga ipsam
-        voluptate laboriosam, illum possimus dignissimos doloremque?
-        Accusantium, voluptates quidem veniam odit qui harum nihil fugit omnis
-        magni sint explicabo molestiae facilis illo, vero iure rerum ea.
-        Reiciendis amet officia rem. Voluptate id animi sit sapiente rem
-        repellendus quod nostrum vitae dolore ea voluptates doloremque ex
-        consequuntur dignissimos repudiandae, delectus facilis modi, ipsum
-        quibusdam magnam provident blanditiis culpa quos! Itaque id vel libero
-        odio reprehenderit. Similique reiciendis molestiae velit laboriosam!
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
